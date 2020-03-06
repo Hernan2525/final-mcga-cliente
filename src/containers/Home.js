822ad228d81081f431f54123ce16f8083da00f51
Hernan2525 <55001087+Hernan2525@users.noreply.github.com>
2020-03-06 02:08:02 -0300

    cliente002

---
+++ src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>MCGA React App</h1>
+        <p>MCGA React App</p>
+      </div>
+    </div>
+  );
+}
